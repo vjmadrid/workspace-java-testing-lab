e49a111f3ab75bd92149f1ee97cd4ea1ccea1455
vjmadrid <vjmadrid@atsistemas.com>
2020-05-01 11:10:39 +0200

    Add Wire Mock

--- demo-testing-api-wiremock/src/test/java/com/acme/testing/api/controller/wiremock/GreetingRestControllerWireMockWithServerTest.java
+++ demo-testing-api-wiremock/src/test/java/com/acme/testing/api/controller/wiremock/GreetingRestControllerWireMockWithServerTest.java
@@ -1,28 +1,27 @@
 package com.acme.testing.api.controller.wiremock;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.configureFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
-
-import java.net.URL;
 
 import org.apache.http.HttpStatus;
 import org.hamcrest.Matchers;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
+import org.springframework.http.MediaType;
 
 import com.acme.testing.api.constant.GreetingRestApiConstant;
 import com.acme.testing.api.testing.util.APITestUtil;
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.github.tomakehurst.wiremock.WireMockServer;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
-public class GreetingRestControllerWireMockTest {
+public class GreetingRestControllerWireMockWithServerTest {
 
 	private static final String PATH_DATA_BODY = "content";
 	
@@ -34,38 +33,53 @@ public class GreetingRestControllerWireMockTest {
 		return APITestUtil.inputStreamToString(GreetingRestControllerWireMockTest.class.getResourceAsStream(file));
 	}
 	
-	public int PORT;
+	public int PORT = 8089;
 	
 	public int HTTPS_PORT;
 	
-	private URL base;
+	private static WireMockServer wireMockServer;
 	
-	@Rule
-	public WireMockRule wireMockRule = new WireMockRule(wireMockConfig().dynamicPort().dynamicHttpsPort());
-
-
 	@Before
 	public void setUp() throws Exception {
-		PORT = wireMockRule.port();
-		HTTPS_PORT = wireMockRule.httpsPort();
+		System.out.println("*** SET UP ***");
+		wireMockServer = new WireMockServer(wireMockConfig().port(PORT));
+		
+        wireMockServer.start();
+        
+        configureFor("localhost", PORT);
+        
+        // *** Other Configuration **
+        //configureFor("wiremock.host", 8090);
+        //configureFor("tomcat.host", 8080, "/wiremock");
+        
+        stubFor(get(urlEqualTo(GreetingRestApiConstant.MAPPING)).willReturn(
+          aResponse().withStatus(200)
+            .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+            .withBody(JSON_ORIGIN)));
+        
+        // *** Wild REturn **
+        /*
+         * 	.willReturn(ok()));
+         *  .willReturn(okJson("{ \"message\": \"Test\" }")));
+         *  .willReturn(unauthorized()));
+         *  .willReturn(status(418)));
+         */
+        
+        RestAssured.port = PORT;
 	}
 	
 	@After
     public void tearDown() throws Exception {
-      
+        wireMockServer.stop();
     }
 
 	@Test
 	public void whenCallAGreetingDefault_thenReturnGreetingDefaultBody() throws Exception {
 
-		base = new URL("http://localhost:" + PORT + GreetingRestApiConstant.MAPPING);
-
 		String EXPECTED_CONTENT_BODY = String.format(GreetingRestApiConstant.TEMPLATE_MESSAGE,
 				GreetingRestApiConstant.DEFAUL_VALUE_PK);
-		
-		stubFor(get(urlEqualTo(GreetingRestApiConstant.MAPPING)).willReturn(okJson(JSON_ORIGIN)));
 
-		RestAssured.get(base.toString()).then()
+		RestAssured.get(GreetingRestApiConstant.MAPPING).then()
 				.statusCode(HttpStatus.SC_OK).contentType(ContentType.JSON)
 				.body(PATH_DATA_BODY, Matchers.equalTo(EXPECTED_CONTENT_BODY));
 	}
---
+++ demo-testing-api-wiremock/src/test/java/com/acme/testing/api/util/APITestUtil.java
@@ -0,0 +1,17 @@
+package com.acme.testing.api.util;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class APITestUtil {
+	
+	protected APITestUtil() {
+		throw new IllegalStateException("APITestUtil");
+	}
+	
+	public static String inputStreamToString(InputStream is) {
+        Scanner s = new Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
+    }
+	
+}
